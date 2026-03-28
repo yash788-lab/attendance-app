b38f9ee6302b6f506eeaf89cae733f0e98f9c368
yash788-lab <yash78ky@gmail.com>
2026-03-28 18:32:47 +0530

    database

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///attendance.db")
+    db_url = os.getenv("DATABASE_URL")
+
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///attendance.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
